31b0607936173165d0976790e2c64194833c7551
Veronelli <facu2000veronelli@outlook.com>
2023-09-15 09:50:05 -0300

    add dinamic url

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,9 +2,9 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const ProductItem = () => {
-    const {query} = useRouter();
+    const {query: {id}} = useRouter();
 
-    return <div>Esta es la pagina del producto: {query.id}</div>
+    return <div>Esta es la pagina del producto: {id}</div>
 }
 
 export default ProductItem
