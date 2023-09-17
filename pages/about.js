4e4181ccb61fa92edce1031cd5a5494e4ed48935
Veronelli <facu2000veronelli@outlook.com>
2023-09-17 10:45:24 -0300

    add navbar and json

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {Navbar} from "../components/Navbar/Navbar";
 
 const About = ()=>{
 	return (
 		<div>
-		Esta pagina es about
+			<Navbar/>
+			<p>
+				Esta pagina es about
+			</p>
 		</div>
 	)
 }
