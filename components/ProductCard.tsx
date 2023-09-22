b3b5118f2022cce006e715241cbde0caa71930bd
Veronelli <facu2000veronelli@outlook.com>
2023-09-22 09:26:10 -0300

    add page product

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,19 +1,20 @@
 function ProductCard({ product }) {
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-100 dark:border-gray-200">
-            <a href="#">
+            <a href={`product/${product.id}`}>
                 <img className="rounded-t-lg w-full" src={product.image} alt="" />
             </a>
             <div className="p-5">
-                <a href="#">
+                <a href={`product/${product.id}`}>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-700">{product.name}</h5>
                 </a>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-full">
+                <a href={`product/${product.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-full">
                     <b>${product.price}</b>
                     <svg className="w- h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </a>
+                
             </div>
         </div>
     )
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import DataBase from 'databases/db'
+import Database from 'databases/db';
 
 const ProductItem = () => {
-    const {query: {id}} = useRouter();
+    const { query: { id } } = useRouter();
+    const dataBase = new DataBase();
+    const [product, setProduct] = React.useState({});
 
-    return <div>Esta es la pagina del producto: {id}</div>
+    dataBase.getById(id).then(res => {
+        setProduct(res);
+    })
+    return (
+        <div className='flex mt-11 justify-around w-full'>
+            {product &&
+                <>
+                    <div style={{ width: "300px" }} className='flex mr-7 border-2 border-l-zinc-200 justify-center'>
+                        <img src={product.image}></img>
+                    </div>
+                    <div className='flex-1 flex flex-col'>
+                        <div className='flex flex-1 flex-col'>
+                            <div>
+                                <h2 className='text-lg font-bold'>{product.name}</h2>
+
+                                <span className='text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-orange-200 text-orange-700 rounded-full'>SKU: {product.sku}</span>
+                            </div>
+                            <div className='pt-4'>
+                                {product.attributes && <p>{product?.attributes?.description}</p>}
+                            </div>
+                        </div>
+
+                        <a href={`product/${product.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-full">
+                            <b>COMPRAR</b>
+                            <svg className="w- h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </>}
+        </div>
+    )
+
 }
 
 export default ProductItem
